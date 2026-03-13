5952b1787d03b94c83ddf413c1cd4d94716d1c15
Yuri Tihonov <tihonov-yuri@mail.ru>
2026-03-14 02:43:46 +0300

    Implement multiline strings alignment

--- 02_PushPip/twocows.py
+++ 02_PushPip/twocows.py
@@ -2,6 +2,20 @@ import argparse
 from cowsay import cowsay
 
 
+def halign_bot(text_l: str, text_r: str) -> str:
+    """Align two multiline strings by the bottom."""
+    lines_l = text_l.split("\n")
+    lines_r = text_r.split("\n")
+    wl = max(map(len, lines_l))
+    hl = len(lines_l)
+    hr = len(lines_r)
+
+    pad_l = (hr - hl) * [""] + lines_l
+    pad_r = (hl - hr) * [""] + lines_r
+    lines_joined = list(map(lambda l, r: f"{l:{wl}} {r}", pad_l, pad_r))
+    return "\n".join(lines_joined)
+
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-e", type=str, default="oo")
@@ -19,5 +33,4 @@ args = parser.parse_args()
 cow1 = cowsay(args.msg1, cow=args.f, eyes=args.e, wrap_text=not args.n)
 cow2 = cowsay(args.msg2, cow=args.F, eyes=args.E, wrap_text=not args.N)
 
-print(cow1)
-print(cow2)
+print(halign_bot(cow1, cow2))
