5ed15006ab7fd074c1b7dac31906674d9a4e0a05
sokomadeyo1 <tihonov-yuri@mail.ru>
2026-03-16 01:38:04 +0300

    Twocows repl (#3)
    * Create environment
    
    * Rewrite twocows using OOP
    
    * Add cowthink
    
    * Add tongue
    
    * Remove hardcoded eyes
    
    * Add repl base that supports cowsay command
    
    * Add cowthink to repl
    
    * Add docstrings for "help" commands
    
    * Add make_bubble command
    
    * Add simple completions with no checking
    
    * Add completion checking
    
    * Add eyes and tongue args checking

---
+++ 04_MergetoolCommandline/repl.py
@@ -0,0 +1,121 @@
+import cmd
+from shlex import split
+
+from cowsay import Option, list_cows, make_bubble
+from twocows import Cow, twocows, twocows_think
+
+
+class CmdCows(cmd.Cmd):
+    def do_list_cows(self, arg):
+        """list_cows
+        List available animals.
+        """
+        for cow in list_cows():
+            print(cow)
+
+    def do_make_bubble(self, arg):
+        """make_bubble text
+        Print text, putting it inside a bubble.
+        """
+        print(make_bubble(arg))
+
+    def do_cowsay(self, arg):
+        """cowsay msg1 [animal {param=value}] reply msg2 [animal {param=value}]
+        Print two animals' dialogue.
+
+        arguments:
+            msg(1,2)    each animal's lines
+            animal      animal file, see list_cows
+            parameters:
+                eyes
+                tongue
+        """
+        args = split(arg)
+        try:
+            delim = args.index("reply")
+        except ValueError:
+            self.do_help("cowsay")
+            return
+        if delim == 0 or delim == len(args) - 1:
+            self.do_help("cowsay")
+
+        try:
+            Cow1 = self.parse_cow_args(args[:delim])
+            Cow2 = self.parse_cow_args(args[delim + 1 :])
+            print(twocows(Cow1, Cow2))
+        except ValueError:
+            self.do_help("cowsay")
+
+    def do_cowthink(self, arg):
+        """cowthink msg1 [animal {param=value}] reply msg2 [animal {param=value}]
+        Print two animals' internal monologues.
+
+        arguments:
+            msg(1,2)    each animal's lines
+            animal      animal file, see list_cows
+            parameters:
+                eyes
+                tongue
+        """
+        args = split(arg)
+        try:
+            delim = args.index("reply")
+        except ValueError:
+            self.do_help("cowthink")
+            return
+        if delim == 0 or delim == len(args) - 1:
+            self.do_help("cowthink")
+
+        try:
+            Cow1 = self.parse_cow_args(args[:delim])
+            Cow2 = self.parse_cow_args(args[delim + 1 :])
+            print(twocows_think(Cow1, Cow2))
+        except ValueError:
+            self.do_help("cowthink")
+
+    def do_EOF(self, arg):
+        return -1
+
+    def complete_cowsay(self, text, line, begin, end):
+        # check if the parameter under the cursor is cow:
+        # either the current parameter should have index 2
+        # or the one before the previous should be "reply"
+        arg_idx = len(split(line[:begin]))
+        if arg_idx != 2:
+            if arg_idx < 2:
+                return []
+            args = split(line)
+            if args[arg_idx - 2] != "reply":
+                return []
+
+        cows = list_cows()
+        return [cow for cow in cows if cow.startswith(text)]
+
+    def complete_cowthink(self, text, line, begin, end):
+        return self.complete_cowsay(text, line, begin, end)
+
+    def parse_cow_args(self, args):
+        msg = args[0]
+        cow = "default"
+        if len(args) > 1:
+            cow = args[1]
+        if any(["=" not in arg for arg in args[2:]]):
+            raise ValueError
+        params = {
+            param: value
+            for (param, value) in [
+                (arg.split("=")[0], arg.split("=")[1]) for arg in args[2:]
+            ]
+        }
+        eyes = Option.eyes
+        if "eyes" in params.keys():
+            eyes = params["eyes"]
+        tongue = Option.tongue
+        if "tongue" in params.keys():
+            tongue = params["tongue"]
+        return Cow(msg, cow=cow, eyes=eyes, tongue=tongue)
+
+
+if __name__ == "__main__":
+    cmd = CmdCows()
+    cmd.cmdloop()
---
+++ 04_MergetoolCommandline/twocows.py
@@ -0,0 +1,74 @@
+import argparse
+from dataclasses import dataclass
+
+from cowsay import Option, cowsay, cowthink
+
+
+def halign_bot(text_l: str, text_r: str) -> str:
+    """Align two multiline strings by the bottom."""
+    lines_l = text_l.split("\n")
+    lines_r = text_r.split("\n")
+    wl = max(map(len, lines_l))
+    hl = len(lines_l)
+    hr = len(lines_r)
+
+    pad_l = (hr - hl) * [""] + lines_l
+    pad_r = (hl - hr) * [""] + lines_r
+    lines_joined = list(map(lambda l, r: f"{l:{wl}} {r}", pad_l, pad_r))
+    return "\n".join(lines_joined)
+
+
+@dataclass
+class Cow:
+    msg: str
+    cow: str = "default"
+    eyes: str = Option.eyes
+    tongue: str = Option.tongue
+    wrap: bool = True
+
+    def __str__(self):
+        return cowsay(
+            self.msg,
+            cow=self.cow,
+            eyes=self.eyes,
+            tongue=self.tongue,
+            wrap_text=self.wrap,
+        )
+
+    def think(self):
+        return cowthink(
+            self.msg,
+            cow=self.cow,
+            eyes=self.eyes,
+            tongue=self.tongue,
+            wrap_text=self.wrap,
+        )
+
+
+def twocows(cow1: Cow, cow2: Cow) -> str:
+    return halign_bot(str(cow1), str(cow2))
+
+
+def twocows_think(cow1: Cow, cow2: Cow) -> str:
+    return halign_bot(cow1.think(), cow2.think())
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-e", type=str, default=Option.eyes)
+    parser.add_argument("-f", type=str, default="default")
+    parser.add_argument("-n", action="store_true")
+    parser.add_argument("msg1", type=str)
+
+    parser.add_argument("-E", type=str, default=Option.eyes)
+    parser.add_argument("-F", type=str, default="default")
+    parser.add_argument("-N", action="store_true")
+    parser.add_argument("msg2", type=str)
+
+    args = parser.parse_args()
+
+    cow1 = Cow(args.msg1, cow=args.f, eyes=args.e, wrap=not args.n)
+    cow2 = Cow(args.msg2, cow=args.F, eyes=args.E, wrap=not args.N)
+
+    print(twocows(cow1, cow2))
