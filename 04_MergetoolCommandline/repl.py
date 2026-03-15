a4b2858fff15f0e2fc15aa2b0207100ef9db7476
Yuri Tihonov <tihonov-yuri@mail.ru>
2026-03-16 00:44:23 +0300

    Add repl base that supports cowsay command

---
+++ 04_MergetoolCommandline/repl.py
@@ -0,0 +1,56 @@
+import cmd
+from shlex import split
+
+from cowsay import Option, list_cows
+from twocows import Cow, twocows
+
+
+class CmdCows(cmd.Cmd):
+    def do_list_cows(self, arg):
+        for cow in list_cows():
+            print(cow)
+
+    def do_make_bubble(self, arg):
+        pass
+
+    def do_cowsay(self, arg):
+        args = split(arg)
+        try:
+            delim = args.index("reply")
+        except ValueError:
+            self.do_help("cowsay")
+            return
+        if delim == 0 or delim == len(args) - 1:
+            self.do_help("cowsay")
+
+        Cow1 = self.parse_cow_args(args[:delim])
+        Cow2 = self.parse_cow_args(args[delim + 1 :])
+        print(twocows(Cow1, Cow2))
+
+
+    def do_EOF(self, arg):
+        return -1
+
+    def parse_cow_args(self, args):
+        msg = args[0]
+        cow = "default"
+        if len(args) > 1:
+            cow = args[1]
+        params = {
+            param: value
+            for (param, value) in [
+                (arg.split("=")[0], arg.split("=")[1]) for arg in args[2:]
+            ]
+        }
+        eyes = Option.eyes
+        if "eyes" in params.keys():
+            eyes = params["eyes"]
+        tongue = Option.tongue
+        if "tongue" in params.keys():
+            tongue = params["tongue"]
+        return Cow(msg, cow=cow, eyes=eyes, tongue=tongue)
+
+
+if __name__ == "__main__":
+    cmd = CmdCows()
+    cmd.cmdloop()
